524f99986bbb64731ff0492dfcf860dd7258ce85
Gitanshu sankhla <lusifer0418@gmail.com>
2023-09-18 18:36:31 +0530

    build first server

---
+++ app.js
@@ -0,0 +1,10 @@
+// Build first server
+
+// const http = require('http');
+
+// const server = http.createServer((req, res)=>{
+//     console.log(res);
+//     return '<h1>Hello world</h1>'
+// });
+
+// server.listen(3000);
