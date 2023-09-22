f9712130cde16e5ff7bb1cc3bff991b0dff444f7
Gitanshu sankhla <lusifer0418@gmail.com>
2023-09-22 12:45:36 +0530

    create first express app

---
+++ #2 Express js/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log('On another middleware')
+    next()
+})
+
+const server = http.createServer(app);
+
+server.listen(3000);
