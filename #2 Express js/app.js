a00dafe173c7d99fec4be1a08593644b3a89f277
Gitanshu sankhla <lusifer0418@gmail.com>
2023-09-23 22:56:39 +0530

    built an form

--- #2 Express js/app.js
+++ #2 Express js/app.js
@@ -1,16 +1,22 @@
 const express = require('express');
-const http = require('http');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();
+const _form = `
+<br>
+<form action='done' method='POST'>
+    <label for='name'>Name:</label>
+    <input type='text' name='name' id='name' placeholder='John Doe'>
+    <button type='submit'>Submit</button>
+</form>
+`
+
+app.use('/done',(req,res,next)=>{
+    res.send('<h1>Check Your File</h1>')
 })
 
-app.use((req,res,next)=>{
-    console.log('On another middleware')
-    res.send('<h1>Hello there, express here!</h1>')
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello there, express here!</h1>'+'<h2>Fill the Form</h2>' + _form);
 })
 
 app.listen(3000);
