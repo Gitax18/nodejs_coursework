71d1f19e29ee42af57bba7ae02086d92d0018d2f
Gitanshu sankhla <lusifer0418@gmail.com>
2023-09-24 21:16:55 +0530

    refactor code

--- #2 Express js/app.js
+++ #2 Express js/app.js
@@ -6,8 +6,18 @@ const homeRouter = require('./routes/home')
 
 const app = express();
 
+// middleware to parse the incoming request data from form 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(adminRoute);
+
+// middleware for admin routes
+app.use('/admin',adminRoute);
+
+// middleware for home route
 app.use(homeRouter);
+
+// middleware to handle page which are not present
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Error: 404</h1> <h2>Page not Found</h2>')
+})
 
 app.listen(3000);
