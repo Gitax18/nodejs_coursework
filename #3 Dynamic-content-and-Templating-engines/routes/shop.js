2944eb08cc2b7aa0c84124a8ee0c2abd7c3a6d95
Gitanshu sankhla <lusifer0418@gmail.com>
2023-09-25 22:27:01 +0530

    print products received from form

--- #3 Dynamic-content-and-Templating-engines/routes/shop.js
+++ #3 Dynamic-content-and-Templating-engines/routes/shop.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const rootDir = require('../utils/path');
+const adminRoute = require('./admin');
 
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
+    console.table(adminRoute.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 })
 
