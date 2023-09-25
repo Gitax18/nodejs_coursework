920aab0d7c40070da621a5e26c3ebae318b95614
Gitanshu sankhla <lusifer0418@gmail.com>
2023-09-25 22:26:39 +0530

    add product using form

--- #3 Dynamic-content-and-Templating-engines/app.js
+++ #3 Dynamic-content-and-Templating-engines/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // middleware for admin routes
-app.use('/admin',adminRoute);
+app.use('/admin',adminRoute.route);
 
 // middleware for home route
 app.use(shopRouter);
