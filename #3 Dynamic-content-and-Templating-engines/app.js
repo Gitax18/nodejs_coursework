61b4e110d78ee9f319c34b46212336dc1fb27f28
Gitanshu sankhla <lusifer0418@gmail.com>
2023-09-27 12:55:51 +0530

    minor refactor

--- #3 Dynamic-content-and-Templating-engines/app.js
+++ #3 Dynamic-content-and-Templating-engines/app.js
@@ -23,7 +23,7 @@ app.use(shopRouter);
 
 // middleware to handle page which are not present
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404')
 })
 
 app.listen(3000);
