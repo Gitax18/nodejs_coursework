091b73a602c546472121236e9da7b5d7ff33ee8f
Gitanshu sankhla <lusifer0418@gmail.com>
2023-09-30 14:35:05 +0530

    write some client side js

---
+++ #4 PROJECT to-do-app/public/scripts/todo.js
@@ -0,0 +1,24 @@
+const doneBtn = document.querySelectorAll('.todo-done');
+const hamMenuBtn = document.querySelector('.ham');
+const navbar = document.querySelector('.nav-elements');
+
+doneBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        console.log(btn.textContent)
+        if(btn.textContent === '✓') btn.textContent = '✗';
+        else btn.textContent = '✓';
+    
+        btn.closest('.todo-item').classList.toggle('done')
+    })
+})
+
+
+hamMenuBtn.addEventListener('click', ()=>{
+    let type = hamMenuBtn.name;
+
+    if (type === 'menu-outline') hamMenuBtn.name = 'close-outline';
+    else hamMenuBtn.name = 'menu-outline';
+
+    navbar.classList.toggle('active')
+})
+
---
+++ #4 PROJECT to-do-app/public/scripts/validation.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// Container
+const form = document.querySelector('.main-form');
+// Inputs
+const inputTitle = document.querySelector('#title')
+const inputDesc = document.querySelector('#description')
+const checkMain = document.querySelector('#main')
+const checkWork = document.querySelector('#work')
+// Buttons
+const btnSub = document.querySelector('.btn-submit');
+
+console.log(form.action)
+
+function validate(){
+    let validate, valInp, valdesc, valChk;
+    valChk = valInp = valdesc = false;
+
+    if (inputTitle.value.length < 16 && inputTitle.value.length !== 0){
+        valInp = true;
+    } else{
+        alert('Title must be less than 16 or must not be empty')
+    }
+
+    if (inputDesc.value.trim() !== ''){
+        valdesc = true
+    }else {
+        alert('Please fill description')
+    }
+
+    if(checkMain.checked || checkWork.checked){
+        valChk = true
+    } else {
+        alert('Please choose any todo type')
+    }
+
+
+    validate = valInp && valdesc && valChk
+    return validate   
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const checked = validate();
+    if(checked){
+        location.href = form.getAttribute('action')
+    }
+})
