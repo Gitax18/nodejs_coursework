b5493560a20cca05ab560114d879e90aee8e95dd
Gitanshu sankhla <lusifer0418@gmail.com>
2023-10-04 21:39:45 +0530

    add delete router

--- #6 Enhancing Application/routes/admin.js
+++ #6 Enhancing Application/routes/admin.js
@@ -13,6 +13,8 @@ router.post('/edit-product', adminController.postEditProduct);
 
 router.get('/edit-product', adminController.getEditProduct);
 
+router.get('/delete-product', adminController.getDeleteProduct);
+
 router.get('/products', adminController.getProducts);
 
 exports.route = router;
